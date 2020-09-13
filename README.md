8c6879bc819b2f99a45ae9cc265402efe1235eae
rainof <rainof527villa@ezweb.ne.jp>
2020-09-14 00:07:31 +0900

    [update] get a tweet

--- README.md
+++ README.md
@@ -4,6 +4,18 @@
 - Python 3.7
 
 # インストール
-- Twitter API
+- Twitter API関連
 
- `pip install twitter`(sixohsix)
+ `pip install twitter`(sixohsix) 
+
+ `pip install emoji 
+
+# 問題点
+- Twitter API
+  - 180リクエスト/15分
+  - 過去7日間分　[バグが多いみたいだけど，頑張れば過去7日以前も取得できるらしい．](https://qiita.com/jinto/items/60f23a6b5d9603836dab)
+  - 自分のアカウントに紐づいていると凍結の可能性
+  - 同じツイートが抽出されている？
+
+# 留意点
+  - 使用する場合はsetting.pyでkeyやtokenなどを記入してください
--- collect_tweets.py
+++ collect_tweets.py
@@ -1,11 +1,8 @@
+import os,time,re
 import setting
-import os
+import emoji
 
 from twitter import *
-
-#import os,sys,time
-#import re
-#import emoji
 
 # settingファイルからapi keyなどを読み込む
 CN = "ツイート収集API"
@@ -19,79 +16,41 @@ oauth_token, oauth_secret = read_token_file(TWITTER_CREDS)
 
 twitter = Twitter(auth=OAuth(oauth_token, oauth_secret, CK, CS)) # 指定したkeywordでtweetを取得
 
-os.makedirs("TweetData", exist_ok=True)
-#twitter_number = 1 #tweetが入っているファイルの識別子
-
-#stream = TwitterStream(auth=OAuth(oauth_token, oauth_secret, CK, CS)) #新着ツイートを探すstream
-# 普通のTwitter APIで'in_reply_to_status_id'を参照するtwitterの2つを用意する。
-
 #NG words
 #ここでタプルに追加した単語（記号）を含むツイートは収集から除外される。
 #今回は、URLやHashtag、【定期】のように使われるカッコなどを除外とした。
-#check_chara = ('http', '#', '\\', '【', '】')
+check_chara = ('http', '\\', '【', '】')
 
-#Retry MAX
-#retry_max = 15
-#Retry time
-#retry_time = 60
-
-#count = 0
+os.makedirs("TweetData", exist_ok=True)
 
 # 正規表現パターンを構築
-#emoji_pattern = re.compile("["
-#        u"\U0001F600-\U0001F64F"
-#        u"\U0001F300-\U0001F5FF"
-#        u"\U0001F680-\U0001F6FF"
-#        u"\U0001F1E0-\U0001F1FF"
-#                           "]+", flags=re.UNICODE)
-
-#def remove_emoji(src_str):
-#  return ''.join(c for c in src_str if c not in emoji.UNICODE_EMOJI)
-
-
-#in_reply_to_status_idから宛先となるツイートを参照する。
-#def show(_status):
-#  global count
-#  global twitter_number
-  
-#  tf = open('Data/' + str(twitter_number) + '_twitter_data.txt', 'a')
-#  for r in range(retry_max):
-  #言語が日本語、in_reply_to_status_idが明示されており、禁止単語を含まないツイートの場合
-#    if 'in_reply_to_status_id' in _status and not _status['in_reply_to_status_id'] is None and 'text' in _status and check(_status['text']):
-#      try:
-      #ツイートを参照
-#        status=twitter.statuses.show(id=_status['in_reply_to_status_id'])
-#      except:
-#        time.sleep(retry_time)
-#        continue
-      #宛先のツイートが日本語、in_reply_to_status_idが明示されており、禁止単語を含まないツイートの場合
-#      if 'lang' in _status and _status['lang'] == 'ja' and 'text' in status and check(status['text']):
-#        tf.write(remove_emoji(emoji_pattern.sub(r'@emoji', trim(status['text']))))
-#        tf.write('\n')
-#        tf.write(remove_emoji(emoji_pattern.sub(r'@emoji', trim(_status['text']))))
-#        tf.write('\n')
-#        count = count + 1
-#        if count == 50000:
-#          twitter_number = twitter_number + 1
-#          count = 0
-#        break
-#      else:
-#        break
-#    else:
-#      break
-#  tf.close()
-
-#改行を適当に置き換える
-#def trim(text):
-#  return text.replace('\r','-br-').replace('\n','-br-')
+emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"
+        u"\U0001F300-\U0001F5FF"
+        u"\U0001F680-\U0001F6FF"
+        u"\U0001F1E0-\U0001F1FF"
+                           "]+", flags=re.UNICODE)
 
 #禁止文字のチェックを行う
-#def check(text):
-#  for char in check_chara:
-#    if char in text:
-#      return False
-#  return True
+def check(text):
+  for char in check_chara:
+    if char in text:
+      return False
+  return True
 
+#絵文字を除去
+def remove_emoji(src_str):
+  return ''.join(c for c in src_str if c not in emoji.UNICODE_EMOJI)
+
+#改行を適当に置き換える
+def trim(text):
+  return text.replace('\r','-br-').replace('\n','-br-')
+
+def show(_status, _tf):
+  #禁止単語を含まないツイートの場合
+  if check(_status['text']):
+    _tf.write(remove_emoji(emoji_pattern.sub(r'@emoji', trim(_status['text']))))
+    _tf.write('\n')
 
 def main():
   keyword = "#バグ"
@@ -99,15 +58,19 @@ def main():
   list_len = len(tweet_data_list)
 
   while True:
-    with open("TweetData/" + str(list_len + 1) + ".txt", "w") as tf:
-      tweet = twitter.search.tweets(q=keyword)
-      print(tweet)
-      break
-    #statuses = stream.statuses.sample() #ストリームに接続して適当にツイートを読み出す
-
- #   for status in statuses:
- #     #参照に送る
- #     show(status)
+    with open("TweetData/" + str(list_len + 1) + ".txt", "w") as tf: #必ず新ファイルに書き込む
+      try:
+        tweet = twitter.search.tweets(q=keyword, lang="ja") #キーワードを含む，かつ，日本語
+        for status in tweet["statuses"]:
+          show(status, tf)
+        break
+      except TwitterHTTPError as twitter_http_error:
+        if twitter_http_error.response_data["errors"][0]["code"] == 88: #15分間に180リクエスト以上要求した場合制限がかかる
+          time.sleep(60 * 15) #15分待つ
+      except Exception as e:
+        print("新しいエラー")
+        print(e)
+        break
 
 if __name__ == '__main__':
   main()
