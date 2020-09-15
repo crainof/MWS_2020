502968a329625d90ef0f09b2d13db1865da65611
Kaito Mimura <kaito.mimura@nflabs.onmicrosoft.com>
2020-09-15 22:32:45 +0900

    tag_list.py--タグのリストを追加しました

---
+++ mws_2020/tag_list.py
@@ -0,0 +1,39 @@
+#"お名前"と"名"の被りとかがちょっとめんどそう
+
+#対応済
+#amazon、楽天、Cisco, U-NEXT, ヤマケイオンライン、zozotown, facebook, instagram, シネマイレージ
+#Unity, Epic Games, Nintendo accont
+
+ful_name = ["名前","氏名","お名前","フルネーム","Full Name","おなまえ"]
+fst_name = ["First Name","名"]
+lst_name = ["Last Name","姓"]
+red_name = ["ふりがな","フリガナ","カナ氏名"]
+mail_add = ["メールアドレス","Email","E-mail"]
+paswd = ["パスワード","Password"]
+usrid = ["ユーザID","ID","ユーザーネーム","Username"]
+country=["Country","Region","国","地域"]
+company=["Company","お勤め先"]
+gender =["性","性別"]
+birth = ["生年月日","誕生日"]
+tel = ["電話番号","連絡先"]
+addr = ["お住まい","住所","都道府県","市区群", "町村"]
+addr_area =["お住いの地域"] #「お住いの地域」は「お住まい」より危険度さげる? 
+post_code = ["郵便番号"]
+card = ["クレジットカード"]
+
+#ful_name = ["名前","氏名","お名前","フルネーム","Full Name","おなまえ"]
+#fst_name = ["First Name","名"]
+#lst_name = ["Last Name","姓"]
+#red_name = ["ふりがな","フリガナ","カナ氏名"]
+#mail_add = ["メールアドレス","Email","E-mail"]
+#paswd = ["パスワード","Password"]
+#usrid = ["ユーザID","ID","ユーザーネーム","Username"]
+#country=["Country","Region","国","地域"]
+#company=["Company","お勤め先"]
+#gender =["性","性別"]
+#birth = ["生年月日","誕生日"]
+#tel = ["電話番号","連絡先"]
+#addr = ["お住まい","住所","都道府県","市区群", "町村"]
+#addr_area =["お住いの地域"] #「お住いの地域」は「お住まい」より危険度さげる? 
+#post_code = ["郵便番号"]
+#card = ["クレジットカード"]
