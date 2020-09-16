42cd044db936ebc9a9ea389fad2653b418e6b680
Kaito Mimura <kaito.mimura@nflabs.onmicrosoft.com>
2020-09-16 18:14:42 +0900

    all_tagを追加しました

--- mws_2020/tag_list.py
+++ mws_2020/tag_list.py
@@ -4,6 +4,13 @@
 #amazon、楽天、Cisco, U-NEXT, ヤマケイオンライン、zozotown, facebook, instagram, シネマイレージ
 #Unity, Epic Games, Nintendo accont
 
+all_tag = ["名前","氏名","お名前","フルネーム","Full Name","おなまえ",
+           "First Name","名","Last Name","姓",
+           "ふりがな","フリガナ","カナ氏名","メールアドレス","Email","E-mail",
+           "パスワード","Password","ユーザID","ID","ユーザーネーム","Username",
+           "Country","Region","国","地域","Company","お勤め先","性","性別","生年月日","誕生日",
+           "電話番号","連絡先","お住まい","住所","都道府県","市区群", "町村","お住いの地域",
+           "郵便番号","クレジットカード"]
 ful_name = ["名前","氏名","お名前","フルネーム","Full Name","おなまえ"]
 fst_name = ["First Name","名"]
 lst_name = ["Last Name","姓"]
@@ -21,19 +28,26 @@ addr_area =["お住いの地域"] #「お住いの地域」は「お住まい」
 post_code = ["郵便番号"]
 card = ["クレジットカード"]
 
-#ful_name = ["名前","氏名","お名前","フルネーム","Full Name","おなまえ"]
-#fst_name = ["First Name","名"]
-#lst_name = ["Last Name","姓"]
-#red_name = ["ふりがな","フリガナ","カナ氏名"]
-#mail_add = ["メールアドレス","Email","E-mail"]
-#paswd = ["パスワード","Password"]
-#usrid = ["ユーザID","ID","ユーザーネーム","Username"]
-#country=["Country","Region","国","地域"]
-#company=["Company","お勤め先"]
-#gender =["性","性別"]
-#birth = ["生年月日","誕生日"]
-#tel = ["電話番号","連絡先"]
-#addr = ["お住まい","住所","都道府県","市区群", "町村"]
-#addr_area =["お住いの地域"] #「お住いの地域」は「お住まい」より危険度さげる? 
-#post_code = ["郵便番号"]
-#card = ["クレジットカード"]
+#all_tag = {"名前","氏名","お名前","フルネーム","Full Name","おなまえ",
+           #"First Name","名","Last Name","姓",
+           #"ふりがな","フリガナ","カナ氏名","メールアドレス","Email","E-mail",
+           #"パスワード","Password","ユーザID","ID","ユーザーネーム","Username",
+           #"Country","Region","国","地域","Company","お勤め先","性","性別","生年月日","誕生日",
+           #"電話番号","連絡先","お住まい","住所","都道府県","市区群", "町村","お住いの地域",
+           #"郵便番号","クレジットカード"}
+#ful_name = {"名前","氏名","お名前","フルネーム","Full Name","おなまえ"}
+#fst_name = {"First Name","名"}
+#lst_name = {"Last Name","姓"}
+#red_name = {"ふりがな","フリガナ","カナ氏名"}
+#mail_add = {"メールアドレス","Email","E-mail"}
+#paswd = {"パスワード","Password"}
+#usrid = {"ユーザID","ID","ユーザーネーム","Username"}
+#country={"Country","Region","国","地域"}
+#company={"Company","お勤め先"}
+#gender ={"性","性別"}
+#birth = {"生年月日","誕生日"}
+#tel = {"電話番号","連絡先"}
+#addr = {"お住まい","住所","都道府県","市区群", "町村"}
+#addr_area ={"お住いの地域"} #「お住いの地域」は「お住まい」より危険度さげる? 
+#post_code = {"郵便番号"}
+#card = {"クレジットカード"}
