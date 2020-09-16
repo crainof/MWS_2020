b64a79e9d0a9d6ac8b686e1303220e4b59e61447
悠介 飯澤 <yusuke.iizawa@nflabs.onmicrosoft.com>
2020-09-16 18:21:21 +0900

    UI作成 ポップアップで表示

---
+++ mws_2020/main.py
@@ -0,0 +1,33 @@
+﻿# -*- coding: utf-8 -*-
+
+import tkinter
+from tkinter import messagebox
+
+#ボタンクリック実行
+
+def button_click():
+
+    input_value = input_box.get()
+    messagebox.showinfo(input_value,"リスク：〇〇\n 詳細：○○　\n")
+
+
+#ウィンドウ作成
+root = tkinter.Tk()
+root.title("Personal Information Risk Checker")
+root.geometry("360x120")
+
+
+#入力欄の作成
+input_box = tkinter.Entry(width=40)
+input_box.place(x=10, y=50)
+
+#ラベル作成
+input_label = tkinter.Label(text="Input URL")
+input_label.place(x=10, y=20)
+
+#ボタン作成
+button = tkinter.Button(text="Analyze",command=button_click)
+button.place(x=10, y=80)
+
+#ウィンドウの描画
+root.mainloop()
